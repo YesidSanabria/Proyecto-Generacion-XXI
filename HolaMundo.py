db3dded21682d127832ee6d395a5ec90158234e1
DavidCely2000 <judacefo@gmail.com>
2022-09-16 10:34:55 -0500

    Create con Firebase

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,1 +1,15 @@
-print ("Bienvenidos Desarrolladores Generación XXI :)")
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("generacion-xxi-firebase-adminsdk-iwq0c-bc2e550415.json")
+firebase_admin.initialize_app(cred,{'databaseURL':'https://generacion-xxi-default-rtdb.firebaseio.com/'})
+
+ref = db.reference('\Estudiantes')
+ref.set({
+    'id1': {
+        'nombre' : 'David',
+        'apellido' : 'Cely',
+        'telefono' : '3214157461'
+    }
+})
