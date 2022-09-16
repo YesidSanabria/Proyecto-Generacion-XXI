fb756c84ea66de543e1a36e55cbcb921083747df
jsebastian5705 <113190215+jsebastian5705@users.noreply.github.com>
2022-09-16 11:29:44 -0500

    importarDatosExcel

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,11 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import pandas as pd 
 
 cred = credentials.Certificate("generacion-xxi-firebase-adminsdk-iwq0c-bc2e550415.json")
 firebase_admin.initialize_app(cred,{'databaseURL':'https://generacion-xxi-default-rtdb.firebaseio.com/'})
 
-ref = db.reference('\Estudiantes')
+#importar datos 
+baseDatos = pd.read_excel('data\DATOS PRACTICANTES (1) (1).xlsx')
+print(baseDatos)
+
+"""
+ref = db.reference('Estudiantes')
 ref.set({
     'id1': {
         'nombre' : 'David',
@@ -13,3 +19,4 @@ ref.set({
         'telefono' : '3214157461'
     }
 })
+"""
