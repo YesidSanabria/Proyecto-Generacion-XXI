8562bc8ee0b43655af372caec460054508760d69
DavidCely2000 <judacefo@gmail.com>
2022-09-16 16:04:20 -0500

    Carga de datos de excel a firebase

--- Carga de datos.py
+++ Carga de datos.py
@@ -1,18 +1,30 @@
+from email.mime import base
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import pandas as pd 
 import numpy as np
+import os
 
 cred = credentials.Certificate("generacion-xxi-firebase-adminsdk-iwq0c-bc2e550415.json")
 firebase_admin.initialize_app(cred,{'databaseURL':'https://generacion-xxi-default-rtdb.firebaseio.com/'})
 
 # Importar datos de Excel.
-baseDatos = pd.read_excel('data\DATOS PRACTICANTES (1) (1).xlsx')
+file = os.listdir('data')[0]
+# Definir todos los campos numéricos como campos de texto.
+col_types = {
+    'Numero en caso de emergencia':str, 
+    'Cedula':str, 
+    'Celular empresarial':str, 
+    'Celular personal':str,
+    'Celular del tutor':str
+    }
+baseDatos = pd.read_excel(f'data\{file}',dtype=col_types)
 dic = baseDatos.to_dict()
 # Convertir los datos a lista de listas.
 columns = []
-for clave in dic.keys():
+keys = list(dic.keys())
+for clave in keys:
     column = []
     for i in dic[clave].keys():
         column.append(dic[clave][i])
@@ -21,12 +33,15 @@ data = np.array(columns)
 data = data.transpose()
 rows = data.tolist()
 
+# Hasta este punto todos los registros están en una lista individual.
 
-# ref = db.reference('Estudiantes')
-# ref.set({
-#     'id1': {
-#         # 'nombre' : 'David',
-#         'apellido' : 'Cely',
-#         # 'telefono' : '3214157461'
-#     }
-# })
+# Convertir cada registro en el formato JSON para subir  Firebase.
+students = {}
+for register in rows:
+    field = {}
+    cc = register[2]    # Cédula.
+    for i in range(len(register)):
+        field[keys[i]] = str(register[i])
+    students[cc] = field
+ref = db.reference('Estudiantes')
+ref.set(students)
