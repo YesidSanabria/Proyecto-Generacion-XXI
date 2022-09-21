6ca3d020cd6111617818c1816566e867a5615dc0
DavidCely2000 <judacefo@gmail.com>
2022-09-21 17:35:52 -0500

    Excepción - Cuenta existente

--- app/routes.py
+++ app/routes.py
@@ -6,16 +6,14 @@ from flask import Flask
 app = Flask(__name__)
 
 config = {
-
-  "apiKey": "AIzaSyDBP7Is2dfzsIzLA-o222p2K2VxoSsFw0c",
-  "authDomain": "generacion-xxi.firebaseapp.com",
-  "databaseURL": "https://generacion-xxi-default-rtdb.firebaseio.com",
-  "projectId": "generacion-xxi",
-  "storageBucket": "generacion-xxi.appspot.com",
-  "messagingSenderId": "347199104837",
-  "appId": "1:347199104837:web:e859d53b27b23d1024c02c",
-  "measurementId": "G-YNKS2S6VJT"
-
+    "apiKey": "AIzaSyDBP7Is2dfzsIzLA-o222p2K2VxoSsFw0c",
+    "authDomain": "generacion-xxi.firebaseapp.com",
+    "databaseURL": "https://generacion-xxi-default-rtdb.firebaseio.com",
+    "projectId": "generacion-xxi",
+    "storageBucket": "generacion-xxi.appspot.com",
+    "messagingSenderId": "347199104837",
+    "appId": "1:347199104837:web:e859d53b27b23d1024c02c",
+    "measurementId": "G-YNKS2S6VJT"
 }
 
 firebase = pyrebase.initialize_app(config)
@@ -26,36 +24,40 @@ auth = firebase.auth()
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if (request.method == 'POST'):
-            email = request.form['name']
-            password = request.form['password']
-            try:
+        email = request.form['name']
+        password = request.form['password']
+        try:
 #--------------------INICIAR SESION---------------------------------                
-                auth.sign_in_with_email_and_password(email, password)
-                #user_id = auth.get_account_info(user['idToken'])
-                #session['usr'] = user_id
-                return render_template('formulario.html')
-            except:
-                unsuccessful = 'Please check your credentials'
-                return render_template('index.html', umessage=unsuccessful)
+            auth.sign_in_with_email_and_password(email, password)
+            #user_id = auth.get_account_info(user['idToken'])
+            #session['usr'] = user_id
+            return render_template('formulario.html')
+        except:
+            unsuccessful = 'Please check your credentials'
+            return render_template('index.html', umessage=unsuccessful)
     return render_template('index.html')
 
 #----------------------REGISTRARSE-----------------------------
 @app.route('/create_account', methods=['GET', 'POST'])
 def create_account():
     if (request.method == 'POST'):
-            email = request.form['name']
-            password = request.form['password']
+        email = request.form['name']
+        password = request.form['password']
+        try:
             auth.create_user_with_email_and_password(email, password)
             return render_template('index.html')
+        except:
+            unsuccessful = 'Esta cuenta ya existe.'
+            return render_template('create_account.html', umessage=unsuccessful)
     return render_template('create_account.html')
 
 #----------------------REINICIAR CONTRASEÑA----------------------
 @app.route('/forgot_password', methods=['GET', 'POST'])
 def forgot_password():
     if (request.method == 'POST'):
-            email = request.form['name']
-            auth.send_password_reset_email(email)
-            return render_template('index.html')
+        email = request.form['name']
+        auth.send_password_reset_email(email)
+        return render_template('index.html')
     return render_template('forgot_password.html')
 
 @app.route('/home', methods=['GET', 'POST'])
