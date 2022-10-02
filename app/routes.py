d6e3dbab45d1f35786ac5ac423777ed5274fa201
Jorge Medina <jmedinacon1@outlook.com>
2022-10-02 08:03:02 -0500

    OK front jorge

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import render_template
 ####################################################################
 from flask import Flask
 
-#------ Importar archivos propios -------
+# ------ Importar archivos propios -------
 import modules.register as reg
 import modules.login as lg
 import modules.passw as pw
@@ -11,18 +11,24 @@ import modules.admin as ad
 
 app = Flask(__name__)
 
-#--------------------------------------REGISTRO E INICIO DE SESION-------------------------------------
+# --------------------------------------REGISTRO E INICIO DE SESION-------------------------------------
+
+
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     return lg.index()
 
-#----------------------REGISTRARSE-----------------------------
+# ----------------------REGISTRARSE-----------------------------
+
+
 @app.route('/create_account', methods=['GET', 'POST'])
 def create_account():
     return reg.create_account()
 
-#----------------------REINICIAR CONTRASEÑA----------------------
+# ----------------------REINICIAR CONTRASEÑA----------------------
+
+
 @app.route('/forgot_password', methods=['GET', 'POST'])
 def forgot_password():
     return pw.forgot_password()
@@ -32,16 +38,28 @@ def forgot_password():
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
-#--------------------------------------REGISTRO E INICIO DE SESION-------------------------------------
+# --------------------------------------REGISTRO E INICIO DE SESION-------------------------------------
+
+
 @app.route('/admin_view', methods=['GET', 'POST'])
 def admin_view():
     return ad.admin()
 
-#--------------------------------------FORMULARIO-------------------------------------
+# --------------------------------------FORMULARIO-------------------------------------
+
+
 @app.route('/formulario', methods=['GET', 'POST'])
 def formulario():
     return render_template('formulario.html')
 
-#---------------------------------PROGRAMAR ARRIBA DEL IF DE ABAJO----------------------------------
+
+# ---------------------------------PROGRAMAR ARRIBA DEL IF DE ABAJO----------------------------------
 if __name__ == '__main__':
     app.run(debug=True)
+
+# ---------------------------------Propuesta ADMIN JM -------------------------------------
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    return render_template('admin.html')
