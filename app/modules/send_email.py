6c899c4dc84ed7ef9b7f2655da0b54ce5baf7d9c
DavidCely2000 <judacefo@gmail.com>
2022-10-14 15:50:10 -0500

    email

--- app/modules/send_email.py
+++ app/modules/send_email.py
@@ -5,9 +5,11 @@ message = EmailMessage()
 
 email_subject = "Email test from Python" 
 sender_email_address = "sspg.xxi@gmail.com" 
-receiver_email_address = "receiver_email@address.com" 
+receiver_email_address = "judacefo@gmail.com" 
 
 # Configure email headers 
 message['Subject'] = email_subject 
 message['From'] = sender_email_address 
 message['To'] = receiver_email_address
+
+message.set_content("Este es un correo de prueba enviado desde python")
