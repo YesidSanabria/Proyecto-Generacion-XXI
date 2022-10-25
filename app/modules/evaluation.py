462f04050e8403d50669a6bbea2b425dbdab990a
DavidCely2000 <judacefo@gmail.com>
2022-10-25 11:52:29 -0500

    Codigo de verificación evaluación

--- app/modules/evaluation.py
+++ app/modules/evaluation.py
@@ -1,5 +1,6 @@
 from flask import render_template, request
 import random
+import modules.send_email as mail
 
 def evaluation():
     if request.method == 'POST':
@@ -8,6 +9,6 @@ def evaluation():
         code = ''
         for d in range(6):
             code += str(random.randint(0,9))
-        print(code)
+        mail.send_mail(email,code)
         return render_template('confirm.html', code=code, email=email, role=role)
     return render_template('evaluation.html')
