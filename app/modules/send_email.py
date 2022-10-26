a7a9329aa9769443eb4dab96d81d33aacc84fd53
jsebastian5705 <113190215+jsebastian5705@users.noreply.github.com>
2022-10-26 08:51:10 -0500

    cambioCorreoAdmin

--- app/modules/send_email.py
+++ app/modules/send_email.py
@@ -7,13 +7,13 @@ def send_mail(email,codVer):
     #configuracion del obejto
     rt.app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     rt.app.config['MAIL_PORT'] = 465
-    rt.app.config['MAIL_USERNAME'] = 'ingsebastianherrerau@gmail.com'
-    rt.app.config['MAIL_PASSWORD'] = 'xhpqhmkyinxwdtqv'
+    rt.app.config['MAIL_USERNAME'] = 'sspg.xxi@gmail.com'
+    rt.app.config['MAIL_PASSWORD'] = 'kerjzfaqfgxbobyn'
     rt.app.config['MAIL_USE_TLS'] = False
     rt.app.config['MAIL_USE_SSL'] = True
                     
     #crea el objeto mail
     mail=Mail(rt.app)
                     
-    msg = Message(codVer,sender='ingsebastianherrerau@gmail.com',recipients=[email])
+    msg = Message(codVer,sender='sspg.xxi@gmail.com',recipients=[email])
     mail.send(msg)
