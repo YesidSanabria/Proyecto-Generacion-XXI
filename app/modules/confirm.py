dfd661d1c382121099320100e3600399a01c2ec6
YesidSanabria <80014227+YesidSanabria@users.noreply.github.com>
2022-12-07 14:20:15 -0500

    EvaluacionTutor

--- app/modules/confirm.py
+++ app/modules/confirm.py
@@ -1,10 +1,13 @@
 from flask import render_template, request
+import modules.crud as crud
 
 def confirm():
     if request.method == 'POST':
         role = request.form['role']
         email = request.form['email']
-        code = request.form['code']
+        code = request.form['code']       
+        yave = request.form["yave"] 
+        usera = crud.getStudentInfo(yave)     
         code_input = request.form['code_input']
         try:
             user = request.form['user']
@@ -12,9 +15,9 @@ def confirm():
             user = ''
         if code == code_input:
             if role == 'docente':
-                return render_template('teacher_ev.html', email=email, user=user)
+                return render_template('teacher_ev.html', email=email, user=user, datauser=usera)
             elif role == 'líder':
-                return render_template('boss_ev.html', email=email, user=user)
+                return render_template('boss_ev.html', email=email, user=user, datauser=usera)
             else:
                 return render_template('create_account.html', email=email)
         else:
