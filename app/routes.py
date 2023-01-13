c9cd3622522612f178d752bdd49452aa21b6ec5c
Jorge Medina Contreras <71410924+jmedina9@users.noreply.github.com>
2023-01-13 10:24:06 -0500

    ok

--- app/routes.py
+++ app/routes.py
@@ -126,19 +126,19 @@ def dashboard_ev():
     
 # ---------------------------------PROGRAMAR ARRIBA DEL IF DE ABAJO----------------------------------
 
-if __name__ == '__main__':
-    server_name = app.config['SERVER_NAME']
-    if server_name and ':' in server_name:
-        host, port = server_name.split(":")
-        port = int(port)
-    else:
-        port = 8000
-        host = "localhost"
-    mail.init_app(app)
-    app.run(host=host, port=port, debug=True)
+# if __name__ == '__main__':
+#     server_name = app.config['SERVER_NAME']
+#     if server_name and ':' in server_name:
+#         host, port = server_name.split(":")
+#         port = int(port)
+#     else:
+#         port = 8000
+#         host = "localhost"
+#     mail.init_app(app)
+#     app.run(host=host, port=port, debug=True)
   
 
 
-# if __name__ == '__main__':
-#     mail.init_app(app)
-#     app.run(debug=True)
+if __name__ == '__main__':
+    mail.init_app(app)
+    app.run(debug=True)
